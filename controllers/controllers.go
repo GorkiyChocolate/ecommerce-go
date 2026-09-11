ebeded9fedd764401c5ce65619e25510a75bd143
gorkiy chocolate <beybamain@gmail.com>
2026-09-11 13:28:20 +0500

    Implement user signup functionality and add error handling in controllers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"ecommerce-go/models"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var UserCollection *mongo.Collection = database.UserData(database.Client, "Users")
+var ProductCollection *mongo.Collection = database.ProductData(database.Client, "Products")
+var Validate = validator.New()
 
 func HashPassword(password string) string {
 
@@ -11,6 +24,50 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 }
 
 func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already "})
+		}
+
+		count, err := UserCollection.CountDocument(ctx, bson.M{"phone": user.Phone })
+
+		defet cancel()
+
+		if err!= nil{
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return 
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "this phone number is already use"
+			})
+			return
+		}
+	}
 
 }
 
--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("Can`t find the product")
+	ErrCantDecodeProducts = errors.New("Can`t decode the product")
+	ErrUserIdIsNotValid   = errors.New("Users id is not valid")
+	ErrCantUpdateUser     = errors.New("Cant update user")
+	ErrCantRemoveItem     = errors.New("Cant remove item")
+	ErrCantGetItem        = errors.New("Cant get the item")
+	ErrCantBuyCartItem    = errors.New("cant buy cart item")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
